05cd509a2060deabf6941100695851a34232174c
Jeancarlos Félix Guevara <jfelixg2324@politecnics.barcelona>
2025-02-05 16:48:40 +0100

    asd
    asd

--- tienda.js
+++ tienda.js
@@ -12,3 +12,21 @@ function drop(ev) {
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
 }
+
+document.querySelectorAll('.skin').forEach(skin => {
+    const img = skin.querySelector('.skin-img');
+    const video = skin.querySelector('.skin-video');
+
+    skin.addEventListener('mouseenter', () => {
+        img.style.display = 'none';
+        video.style.display = 'block';
+        video.play();
+    });
+
+    skin.addEventListener('mouseleave', () => {
+        img.style.display = 'block';
+        video.style.display = 'none';
+        video.pause();
+        video.currentTime = 0;
+    });
+});
